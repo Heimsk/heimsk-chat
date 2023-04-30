b74d89c703a1b7501c8c3e4910a3090908ebf84a
heimsk <henriquemuller936@gmail.com>
2023-04-30 13:29:25 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/Home.module.css";
+import React from "react";
+
+export default class HomePage extends React.Component {
+  public render() {
+    return (
+      <>
+        <h1>Hello World</h1>
+      </>
+    );
+  }
+}
