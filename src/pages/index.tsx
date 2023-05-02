f846f1397dd93281c06558d79130ab6cb1408759
heimsk <henriquemuller936@gmail.com>
2023-05-02 17:14:55 -0300

    feat: inserted components in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/Home.module.scss";
 import React from "react";
+import Layout from "../components/Layout";
 
 export default class HomePage extends React.Component {
   public render() {
     return (
       <>
-        <h1>Hello World</h1>
+        <Layout />
       </>
     );
   }
