88aae50570c631d0eff9d530e8384997f6e74ad9
Heimsk <64988539+Heimsk@users.noreply.github.com>
2023-05-22 20:53:10 -0300

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -9,7 +9,7 @@ export default class Message extends React.Component {
                 <div id={styles.avatar}></div>
                 <div id={styles.infos}>
                     <div id={styles.top}>
-                        <span id={styles.name}>Heimsk</span>
+                        <span id={styles.name}>Alguem</span>
                         <span id={styles.timestamp}>há 3 horas</span>
                     </div>
                     <div id={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas dolores doloremque vel esse commodi beatae facere autem. Ducimus tempora cum, perspiciatis nemo, exercitationem possimus aliquid repudiandae, facilis quis commodi amet.</div>
